43db72613d99345214ea09b0051678190f4d0dd5
Ian <developer.kss@gmail.com>
2017-03-09 06:59:20 +0900

    NFC intent received

--- app/src/main/java/com/nalive/tnfc/MainActivity.java
+++ app/src/main/java/com/nalive/tnfc/MainActivity.java
@@ -1,9 +1,13 @@
 package com.nalive.tnfc;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,4 +30,33 @@ public class MainActivity extends AppCompatActivity {
             tv.setText("NFC is ok");
         }
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        Toast.makeText(this, "NFC intent received", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter filters[] = new IntentFilter[]{};
+
+        nfc.enableForegroundDispatch(this, pendingIntent, filters, null);
+
+    }
+
+    @Override
+    protected void onPause() {
+        nfc.disableForegroundDispatch(this);
+
+        super.onPause();
+    }
 }
+
