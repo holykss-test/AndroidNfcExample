75935ec90a772adaa3c6ad957942e0879e3fc8d2
Ian <developer.kss@gmail.com>
2017-03-09 08:52:33 +0900

    write preparing

--- app/src/main/java/com/nalive/tnfc/MainActivity.java
+++ app/src/main/java/com/nalive/tnfc/MainActivity.java
@@ -6,14 +6,23 @@ import android.content.IntentFilter;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     TextView tv;
     private NfcAdapter nfc;
 
@@ -90,6 +99,69 @@ public class MainActivity extends AppCompatActivity {
         nfc.disableForegroundDispatch(this);
 
         super.onPause();
+    }
+
+    private void formatTag(@NonNull Tag tag, NdefMessage message) {
+        try {
+            NdefFormatable formatable = NdefFormatable.get(tag);
+
+            if (formatable == null) {
+                Toast.makeText(this, "Tag is not ndef formatable", Toast.LENGTH_SHORT).show();
+            }
+
+            formatable.connect();
+            formatable.format(message);
+            formatable.close();
+        } catch (Exception e) {
+            Log.e("Format TAG", e.getMessage());
+        }
+    }
+
+    private void writeMessage(@NonNull Tag tag, NdefMessage message) {
+
+        Ndef ndef = Ndef.get(tag);
+
+        try {
+
+            if (ndef == null) {
+                formatTag(tag, message);
+            } else {
+                ndef.connect();
+
+                if (!ndef.isWritable()) {
+                    Toast.makeText(this, "Tag is not wratable", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+            Log.e("Write Tag", e.getMessage());
+        } finally {
+            if (ndef != null) {
+                try {
+                    ndef.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private NdefRecord createTextRecord(String content) {
+        try {
+            return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT,
+                    new byte[]{}, content.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private NdefMessage createMessage(String text) {
+        NdefMessage message = new NdefMessage(new NdefRecord[]{
+                createTextRecord(text)
+        });
+
+        return message;
     }
 }
 
