9f4a01c4d7d971a1ab863da23434b2fbbe16d1d7
Ian <developer.kss@gmail.com>
2017-03-09 06:45:02 +0900

    tv

--- app/src/main/java/com/nalive/tnfc/MainActivity.java
+++ app/src/main/java/com/nalive/tnfc/MainActivity.java
@@ -4,7 +4,6 @@ import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
         nfc = NfcAdapter.getDefaultAdapter(this);
         if (nfc == null) {
-            Toast.makeText(this, "No NFC feature", Toast.LENGTH_SHORT).show();
+            tv.setText("No NFC feature");
         } else if (nfc.isEnabled() == false) {
-            Toast.makeText(this, "NFC is disabled", Toast.LENGTH_SHORT).show();
+            tv.setText("NFC is disabled");
         } else {
-            Toast.makeText(this, "NFC is ok", Toast.LENGTH_SHORT).show();
+            tv.setText("NFC is ok");
         }
     }
 }
